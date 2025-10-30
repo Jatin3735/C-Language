287b9bf2e5d966b6c922b7490000245793770d74
Jatin <jatinjangra514@gmail.com>
2025-10-30 16:11:17 +0530

    add two strings with maniplucations

---
+++ concatenation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char str1[20]="Hello",str2[20]="world";
+    int ii=0,i;
+    for(i=0;str1[i]!='\0';i++){
+        ii++;
+    }
+    for(i=0;str2[i]!='\0';i++){
+        str1[ii]=str2[i];
+        ii++;
+    }
+    str1[ii]='\0';
+    printf("%s",str1);
+    return 0;
+}
